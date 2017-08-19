b37b502ad2a7887aa4eaf00f9fc7291ceafddc90
Mingjie Jiang <jiang@mingjie.info>
2017-08-19 14:22:18 -0400

    Login System Test

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+var provider = new firebase.auth.FacebookAuthProvider();
+provider.addScope('user_location');
+provider.addScope('public_profile');
+provider.addScope('user_friends');
+provider.addScope('email');
+provider.addScope('rsvp_event');
+
+
+provider.setCustomParameters({
+    'display': 'redirect'
+});
+
+function fbLogin() {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+
+        console.log(user);
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}

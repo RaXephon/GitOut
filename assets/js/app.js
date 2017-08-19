9033676a48c513a68f9e370667069b9d144df5c3
Mingjie Jiang <jiang@mingjie.info>
2017-08-19 15:12:35 -0400

    Scope

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,26 +11,36 @@ firebase.initializeApp(config);
 
 
 const provider = new firebase.auth.FacebookAuthProvider();
+provider.addScope('user_location');
+provider.addScope('public_profile');
+provider.addScope('user_friends');
+provider.addScope('email');
+provider.addScope('rsvp_event');
 
 function logIn() {
 
     firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
-
-        console.log(user);
+        isLoggedIn(user);
     }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-
         console.log(error);
     });
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        isLoggedIn(firebase.auth().currentUser);
+    } else {
+        isLoggedOut();
+    }
+});
+
+function isLoggedIn(user) {
+    $("login-button").hide();
+    $("#loggedIn").html("Logged in as: " + user.displayName);
+}
+
+function isLoggedOut() {
+    $("login-button").show();
+    $("#loggedIn").html("");
 }

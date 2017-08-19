539d76bcebc0603760292fdbc4f89751e807fc67
Mingjie Jiang <jiang@mingjie.info>
2017-08-19 14:36:26 -0400

    GOogle Login

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,33 +10,22 @@ var config = {
 firebase.initializeApp(config);
 
 
-function fbLogin() {
-    var provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('user_location');
-    provider.addScope('public_profile');
-    provider.addScope('user_friends');
-    provider.addScope('email');
-    provider.addScope('rsvp_event');
+var provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithRedirect(provider);
-
-    firebase.auth().getRedirectResult().then(function (result) {
-        if (result.credential) {
-            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-            var token = result.credential.accessToken;
-            // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user);
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
-}
+firebase.auth().signInWithPopup(provider).then(function (result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    
+    console.log(user);
+}).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+});
